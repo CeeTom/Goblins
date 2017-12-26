78c5cec217367fa5f8853aaacaccdcd0890f24e4
CeeTom <ceetom.github@gmail.com>
2017-12-26 12:05:58 +0000

    added data calls to attack-editor server; begin work on attack-editor UI

--- src/goblins/tools/attack-editor/main.go
+++ src/goblins/tools/attack-editor/main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
+    "goblins/game"
 	"goblins/game/combat"
 	"goblins/game/dataio"
 	"net/http"
@@ -26,6 +27,10 @@ func main() {
 	attacksP := &attacks
 
 	http.Handle("/attacks", serveAttacks(attacksP))
+    http.Handle("/stats", http.HandlerFunc(serveStats))
+    http.Handle("/scalingFuncs", http.HandlerFunc(serveScalingFuncs))
+    http.Handle("/damageTypes", http.HandlerFunc(serveDamageTypes))
+    http.Handle("/statuses", http.HandlerFunc(serveStatuses))
 	http.Handle("/", http.FileServer(http.Dir(*webDir)))
 
 	err = http.ListenAndServe(*host, nil)
@@ -36,19 +41,67 @@ func main() {
 	os.Exit(0)
 }
 
+type IdNamePair struct {
+    Id uint64
+    Name string
+}
+
+func popIdNamePair(tgt *IdNamePair, v game.EnumId) {
+    tgt.Name = v.Name()
+    tgt.Id = v.AsU64()
+}
+
 func serveAttacks(attacksP *[]*combat.Attack) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		bytes, err := json.Marshal(*attacksP)
-		if err != nil {
-			errBytes, err := json.Marshal(err.Error())
-			if err != nil {
-				panic(err.Error())
-			}
-			w.WriteHeader(500)
-			w.Write([]byte(`{"error": `))
-			w.Write(errBytes)
-			w.Write([]byte{'}'})
-		}
-		w.Write(bytes)
+		writeAsJson(w, *attacksP)
 	})
+}
+
+func serveStats(w http.ResponseWriter, r *http.Request) {
+	arr := make([]IdNamePair, len(combat.AllStats))
+	for i, v := range combat.AllStats {
+		popIdNamePair(&arr[i], v)
+	}
+	writeAsJson(w, arr)
+}
+
+func serveScalingFuncs(w http.ResponseWriter, r *http.Request) {
+	arr := make([]IdNamePair, len(combat.AllScalingFuncs))
+	for i, v := range combat.AllScalingFuncs {
+		popIdNamePair(&arr[i], v)
+	}
+	writeAsJson(w, arr)
+}
+
+func serveDamageTypes(w http.ResponseWriter, r *http.Request) {
+	arr := make([]IdNamePair, len(combat.AllDamageTypes))
+	for i, v := range combat.AllDamageTypes {
+		popIdNamePair(&arr[i], v)
+	}
+	writeAsJson(w, arr)
+}
+
+func serveStatuses(w http.ResponseWriter, r *http.Request) {
+	arr := make([]IdNamePair, len(combat.AllStatuses))
+	for i, v := range combat.AllStatuses {
+		popIdNamePair(&arr[i], v)
+	}
+	writeAsJson(w, arr)
+}
+
+func writeAsJson(w http.ResponseWriter, v interface{}) {
+	bytes, err := json.Marshal(v)
+	if err != nil {
+		errBytes, err := json.Marshal(err.Error())
+		if err != nil {
+			panic(err.Error())
+		}
+		w.WriteHeader(500)
+		w.Write([]byte(`{"error": `))
+		w.Write(errBytes)
+		w.Write([]byte{'}'})
+        return
+	}
+    w.Header().Set("Content-type", "application/json")
+	w.Write(bytes)
 }
