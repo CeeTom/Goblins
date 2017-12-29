bc2e3b80787222a69051060249462f4d7d565636
CeeTom <ceetom.github@gmail.com>
2017-12-29 13:43:16 +0000

    add ability to load all breeds from disk

--- src/goblins/game/dataio/breed.go
+++ src/goblins/game/dataio/breed.go
@@ -6,10 +6,65 @@ import (
 	"github.com/pkg/errors"
 	"goblins/game"
 	"io"
+	"io/ioutil"
+	"os"
+	"path"
+	"strings"
 )
 
-// "BREED " 0x00 0x00
-const breedHeader = 0x0000204445455242
+const (
+	// "BREED " 0x00 0x00
+	breedHeader  = 0x0000204445455242
+	BreedFileExt = ".brd"
+)
+
+func ReadAllBreeds(dirname string) ([]*game.Breed, error) {
+	ret := make([]*game.Breed, 0, 64)
+
+	files, err := ioutil.ReadDir(dirname)
+	if err != nil {
+		return nil, errors.Wrap(err, "Couldn't read breed directory entries")
+	}
+
+	for _, fileInfo := range files {
+		// skip directories
+		if !fileInfo.IsDir() {
+			name := path.Join(dirname, fileInfo.Name())
+			if strings.HasSuffix(name, BreedFileExt) {
+				file, err := os.Open(name)
+				if err != nil {
+					msg := fmt.Sprintf("Couldn't open breed file: %s", name)
+					return nil, errors.Wrap(err, msg)
+				}
+				brd, err := ReadBreed(file)
+				if err != nil {
+					msg := fmt.Sprintf("Couldn't read breed file: %s", name)
+					return nil, errors.Wrap(err, msg)
+				}
+				idx := int(brd.Id)
+				if idx >= len(ret) {
+					if idx >= cap(ret) {
+						newCap := cap(ret) * 2
+						if idx >= newCap {
+							newCap = idx + 1
+						}
+						nret := make([]*game.Breed, len(ret), newCap)
+						copy(nret, ret)
+						ret = nret
+					}
+					ret = ret[:idx+1]
+				}
+				if ret[idx] != nil {
+					msg := fmt.Sprintf("Breeds %s and %s have same id %d",
+						ret[idx].Name, brd.Name, idx)
+					return nil, errors.New(msg)
+				}
+				ret[idx] = brd
+			}
+		}
+	}
+	return ret, nil
+}
 
 func ReadBreed(r io.Reader) (*game.Breed, error) {
 	ret := new(game.Breed)
